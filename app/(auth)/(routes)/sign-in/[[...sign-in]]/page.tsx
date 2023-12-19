27718306686f5b378c511438364e3d158ee4e555
Matthew Dsouza <matthewdsouza.one@gmail.com>
2023-12-19 13:54:34 +0530

    added test credentials

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,15 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <>
+      <SignIn />
+    <div>
+      <p>Test Username: jane+clerk_test@example.com</p>
+      <p>Test Password: jane+clerk_test@example.com</p>
+      <p>Confirmation Code: 424242</p>
+
+    </div>
+    </>
+  );
 }
